f53ea586274f6348ba95867c752a1c8ed5d16657
letanatol <letanatol@gmail.com>
2024-01-11 12:41:47 +0100

    feat: add functions gameOver and resetGame

--- hangman/scripts/script.js
+++ hangman/scripts/script.js
@@ -4,12 +4,28 @@ const hangmanImage = document.querySelector('.image-box__image');
 const gameBoxLetters = document.querySelector('.game-box__letters');
 const guessesCounter = document.querySelector('.game-box__guesses-text');
 const keyboard = document.querySelector('.game-box__keyboard');
+const gameModal = document.querySelector('.game-modal');
 
 const maxGuesses = 6;
 let currentAnswer;
 let badAnswers;
 let goodAnswers;
 
+const resetGame = () => {
+  goodAnswers = [];
+  badAnswers = 0;
+
+  hangmanImage.src = `./assets/${badAnswers}.svg`;
+  guessesCounter.innerText = `${badAnswers} / ${maxGuesses}`;
+  keyboard.querySelectorAll('button').forEach((button) => {
+    button.disabled = false;
+    button.dataset.about = true;
+  });
+
+  gameBoxLetters.innerHTML = [...currentAnswer].map(() =>
+    `<li class="letter"></li>`).join(''); // __ __ __ __ __ __ нарисует нижний бортик по длине слова
+  gameModal.classList.remove('show');
+}
 
 const getRandomQuestionAnswer = () => {
   const { answer, hint } = questionAnswer[Math.floor(Math.random() * questionAnswer.length)];
@@ -18,6 +34,18 @@ const getRandomQuestionAnswer = () => {
   currentAnswer = answer.toUpperCase();
   document.querySelector('.game-box__hint-text').innerText = hint;
 
+  resetGame();
+}
+
+const gameOver = (isWon) => {
+  setTimeout(() => {
+    const modalText = isWon ? `You found the word:` : `The correct word was: `;
+
+    gameModal.querySelector('img').src = `./assets/${isWon ? 'modal-won' : 'modal-lost'}.png`;
+    gameModal.querySelector('h4').innerText = `${isWon ? 'Congrats!' : 'Game Over!'}`;
+    gameModal.querySelector('p').innerHTML = `${modalText} <b>${currentAnswer}</b>`;
+    gameModal.classList.add("show");
+  }, 300);
 }
 
 const initGame = (button, letter) => {
