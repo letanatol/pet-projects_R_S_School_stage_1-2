39d7ab4598fed4746c90bdfad4e30036d4de403d
letanatol <letanatol@gmail.com>
2024-01-11 12:11:42 +0100

    feat: add function  getRandomQuestionAnswer

--- hangman/scripts/script.js
+++ hangman/scripts/script.js
@@ -1,1 +1,14 @@
-import { questionAnswer } from "./question-answer.js";
+import questionAnswer from "./question-answer.js";
+
+let currentAnswer;
+
+const getRandomQuestionAnswer = () => {
+  const { answer, hint } = questionAnswer[Math.floor(Math.random() * questionAnswer.length)];
+
+  console.log(answer);
+  currentAnswer = answer.toUpperCase();
+  document.querySelector('.game-box__hint-text').innerText = hint;
+
+}
+
+getRandomQuestionAnswer();
