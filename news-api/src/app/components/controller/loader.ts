0b1e7ed01406c5e13a540314c3ef56d30b1caafb
letanatol <letanatol@gmail.com>
2024-02-25 03:46:04 +0300

    feat: migrate loader to ts

---
+++ news-api/src/app/components/controller/loader.ts
@@ -0,0 +1,53 @@
+interface OptionsInterface {
+  [key: string]: string;
+}
+
+class Loader {
+  private baseLink: string;
+  private options: OptionsInterface;
+
+  constructor(baseLink: string, options: OptionsInterface) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  getResp(
+    { endpoint, options = {} }: { endpoint: string; options?: OptionsInterface },
+    callback: (data: any) => void = () => {
+      console.error('No callback for GET response');
+    }
+  ): void {
+    this.load('GET', endpoint, callback, options);
+  }
+
+  private errorHandler(res: Response): Response {
+    if (!res.ok) {
+      if (res.status === 401 || res.status === 404)
+        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      throw Error(res.statusText);
+    }
+
+    return res;
+  }
+
+  private makeUrl(options: OptionsInterface, endpoint: string) {
+    const urlOptions = { ...this.options, ...options };
+    let url = `${this.baseLink}${endpoint}?`;
+
+    Object.keys(urlOptions).forEach((key) => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
+
+    return url.slice(0, -1);
+  }
+
+  private load<T>(method: string, endpoint: string, callback: (data: T) => void, options: OptionsInterface = {}): void {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then(this.errorHandler)
+      .then((res) => res.json())
+      .then((data: T) => callback(data))
+      .catch((err) => console.error(err));
+  }
+}
+
+export default Loader;
