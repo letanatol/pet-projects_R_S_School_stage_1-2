5fbef5e9960069a73552f6ead57dde93aac71c15
letanatol <letanatol@gmail.com>
2024-03-16 02:48:01 +0300

    feat: RSS-PZ-02 Implement Comprehensive Validation

--- rss-puzzle/src/app/pages/loginPage/loginPage.ts
+++ rss-puzzle/src/app/pages/loginPage/loginPage.ts
@@ -24,6 +24,19 @@ export class LoginPage {
     this.usernameInput.setAttribute('id', 'username');
     this.usernameInput.setAttribute('placeholder', 'Enter your name');
     this.usernameInput.setAttribute('required', '');
+    this.usernameInput.setAttribute(
+      'title',
+      'Enter at least 3 Latin characters including an uppercase letter. Only "-" special characters are allowed.'
+    );
+    this.usernameInput.addEventListener('input', () => this.usernameInput.classList.remove('error'));
+    this.usernameInput.addEventListener('input', () => {
+      const isValid = /^[A-Z][a-zA-Z-]{2,}$/.test(this.usernameInput.value);
+      if (isValid) {
+        this.usernameInput.classList.remove('error');
+      } else {
+        this.usernameInput.classList.add('error');
+      }
+    });
 
     const surnameLabel = document.createElement('label');
     surnameLabel.setAttribute('for', 'surname');
@@ -32,6 +45,19 @@ export class LoginPage {
     this.surnameInput.setAttribute('id', 'surname');
     this.surnameInput.setAttribute('placeholder', 'Enter your surname');
     this.surnameInput.setAttribute('required', '');
+    this.surnameInput.setAttribute(
+      'title',
+      'Enter at least 4 Latin characters including an uppercase letter. Only "-" special characters are allowed.'
+    );
+    this.surnameInput.addEventListener('input', () => this.surnameInput.classList.remove('error'));
+    this.surnameInput.addEventListener('input', () => {
+      const isValid = /^[A-Z][a-zA-Z-]{3,}$/.test(this.surnameInput.value);
+      if (isValid) {
+        this.surnameInput.classList.remove('error');
+      } else {
+        this.surnameInput.classList.add('error');
+      }
+    });
 
     const usernameWrapper = document.createElement('div');
     usernameWrapper.classList.add('input_wrapper');
@@ -46,9 +72,25 @@ export class LoginPage {
     submitButton.setAttribute('type', 'submit');
 
     form.append(usernameWrapper, surnameWrapper, submitButton);
+    form.addEventListener('submit', (event) => this.handleLogin(event));
 
     container.appendChild(form);
 
     return container;
+  }
+
+  private handleLogin(event: Event): void {
+    event.preventDefault();
+    const isValidUsername = /^[A-Z][a-zA-Z-]{2,}$/.test(this.usernameInput.value);
+    if (!isValidUsername) {
+      this.usernameInput.classList.add('error');
+
+      return;
+    }
+
+    const isValidSurname = /^[A-Z][a-zA-Z-]{3,}$/.test(this.surnameInput.value);
+    if (!isValidSurname) {
+      this.surnameInput.classList.add('error');
+    }
   }
 }
