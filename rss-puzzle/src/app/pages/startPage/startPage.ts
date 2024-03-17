32d047db76b51b05570930bf3d285e37379ca32c
letanatol <letanatol@gmail.com>
2024-03-17 22:13:01 +0300

    feat: RSS-PZ-06 Display Personalized Greeting with Name from Local Storage

--- rss-puzzle/src/app/pages/startPage/startPage.ts
+++ rss-puzzle/src/app/pages/startPage/startPage.ts
@@ -1,5 +1,7 @@
+import { UserType } from '@helpers/types';
 import { ButtonComponent } from '@components/buttonComponent';
 import { dispatchCustomEvent } from '@helpers/dispatchCustomEvent';
+import { localStorageService } from '../../helpers/localStorage';
 
 import './startPage.scss';
 
@@ -16,9 +18,19 @@ export class StartPage {
     description.classList.add('description');
     description.textContent = `RSS Puzzle is an interactive mini-game aimed at enhancing English language skills. Assemble sentences from jumbled words, inspired by Lingualeo's Phrase Constructor training. The game integrates various levels of difficulty, hint options, and a unique puzzle-like experience with artwork. Click on words, collect phrases. Words can be drag and drop. Select tooltips.`;
 
+    const greeting = document.createElement('p');
+    greeting.classList.add('greeting');
+
+    const savedUser = localStorageService.getData<UserType>('user');
+
+    if (savedUser) {
+      const { name, surname } = savedUser;
+      greeting.textContent = `Welcome back, ${name} ${surname}!`;
+    }
+
     const logoutButton = new ButtonComponent('Logout', () => dispatchCustomEvent('logout')).createButton();
 
-    container.append(title, description, logoutButton);
+    container.append(title, description, greeting, logoutButton);
 
     return container;
   }
