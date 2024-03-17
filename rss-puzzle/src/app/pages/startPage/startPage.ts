a098193acae3b711d6f3b88a163c77fce980284f
letanatol <100472058+letanatol@users.noreply.github.com>
2024-03-17 20:51:07 +0300

    RSS-PZ-05: Design Start Screen with Application Name and Game Description (#41)

--- rss-puzzle/src/app/pages/startPage/startPage.ts
+++ rss-puzzle/src/app/pages/startPage/startPage.ts
@@ -8,9 +8,17 @@ export class StartPage {
     const container = document.createElement('div');
     container.classList.add('container');
 
+    const title = document.createElement('h1');
+    title.textContent = 'English Puzzle';
+    title.classList.add('title_login');
+
+    const description = document.createElement('p');
+    description.classList.add('description');
+    description.textContent = `RSS Puzzle is an interactive mini-game aimed at enhancing English language skills. Assemble sentences from jumbled words, inspired by Lingualeo's Phrase Constructor training. The game integrates various levels of difficulty, hint options, and a unique puzzle-like experience with artwork. Click on words, collect phrases. Words can be drag and drop. Select tooltips.`;
+
     const logoutButton = new ButtonComponent('Logout', () => dispatchCustomEvent('logout')).createButton();
 
-    container.append(logoutButton);
+    container.append(title, description, logoutButton);
 
     return container;
   }
