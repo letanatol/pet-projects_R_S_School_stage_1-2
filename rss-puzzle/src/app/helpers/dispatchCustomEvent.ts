3704e170798f0aa6b00867b3b80f5f15fce76ad5
letanatol <letanatol@gmail.com>
2024-03-18 15:32:11 +0300

    feat: RSS-PZ-10: Enable Reversible Interaction with Word Cards Between

--- rss-puzzle/src/app/helpers/dispatchCustomEvent.ts
+++ rss-puzzle/src/app/helpers/dispatchCustomEvent.ts
@@ -1,4 +1,6 @@
-export function dispatchCustomEvent(eventName: string): void {
+import { EventTypes } from './types';
+
+export function dispatchCustomEvent(eventName: string | EventTypes): void {
   const customEvent = new CustomEvent(eventName, { bubbles: true });
   window.dispatchEvent(customEvent);
 }
