900102e160b472e5d2919981f5704e5bdeb27aa5
letanatol <100472058+letanatol@users.noreply.github.com>
2024-03-18 13:15:56 +0300

    feat: RSS-PZ-09: Dynamic Sizing of Word Cards Based on Word Length (#46)

--- rss-puzzle/src/app/components/sourceBlock/sourceBlock.ts
+++ rss-puzzle/src/app/components/sourceBlock/sourceBlock.ts
@@ -3,12 +3,20 @@ import './sourceBlock.scss';
 import { shuffleArray } from '@helpers/shuffleArray';
 import { EventTypes } from '@helpers/types';
 
+const WIDTH_CONTAINER = 760;
+
 export class SourceBlock {
   private wrapper: HTMLElement = document.createElement('div');
+
+  private widthSourceBlock = WIDTH_CONTAINER;
 
   private word = state.getRowData().textExample;
 
   constructor() {
+    window.addEventListener(EventTypes.ChangeLevel, () => {
+      this.word = state.getRowData().textExample;
+      this.fillWrapper();
+    });
     window.addEventListener(EventTypes.ChangeRound, () => {
       this.word = state.getRowData().textExample;
       this.fillWrapper();
@@ -18,11 +26,17 @@ export class SourceBlock {
   private fillWrapper(): void {
     if (this.word) {
       this.wrapper.innerHTML = '';
+      const sentenceWithoutSpaces: string = this.word.replace(/\s/g, '');
+      const countLettersOfSentence: number = sentenceWithoutSpaces.length;
       const arrayWords = this.word.split(' ');
       const arrayWordsShuffled = shuffleArray(arrayWords);
 
       arrayWordsShuffled.forEach((word) => {
+        const countLettersOfWord: number = word.length;
+        const densityWord = countLettersOfWord / countLettersOfSentence;
         const wrapperWord = document.createElement('div');
+        const widthWrapperWord = Math.round(this.widthSourceBlock * densityWord);
+        wrapperWord.style.width = `${widthWrapperWord}px`;
         wrapperWord.classList.add('wrapper_word');
         wrapperWord.innerText = word;
         wrapperWord.setAttribute('draggable', 'true');
