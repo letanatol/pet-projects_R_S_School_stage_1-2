0187f3ce37a17525dfc07f455b9e38295176f2e7
letanatol <letanatol@gmail.com>
2024-03-26 01:16:09 +0300

    init: start task async-race

---
+++ async-race/webpack.config.js
@@ -0,0 +1,105 @@
+/**
+ * Webpack main configuration file
+ */
+const path = require('path');
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const environment = require('./configuration/environment');
+
+const templateFiles = fs
+  .readdirSync(environment.paths.source)
+  .filter((file) => ['.html', '.ejs'].includes(path.extname(file).toLowerCase()))
+  .map((filename) => ({
+    input: filename,
+    output: filename.replace(/\.ejs$/, '.html'),
+  }));
+
+const htmlPluginEntries = templateFiles.map(
+  (template) => new HTMLWebpackPlugin({
+    inject: true,
+    hash: false,
+    filename: template.output,
+    template: path.resolve(environment.paths.source, template.input),
+  }),
+);
+
+module.exports = {
+  entry: {
+    app: path.resolve(environment.paths.source, 'app', 'index.ts'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: environment.paths.output,
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.js', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      { test: /\.ts?$/, loader: 'ts-loader' },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: environment.limits.images,
+          },
+        },
+        generator: {
+          filename: 'images/design/[name].[hash:6][ext]',
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: environment.limits.images,
+          },
+        },
+        generator: {
+          filename: 'fonts/[name].[hash:6][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'assets'),
+          to: path.resolve(environment.paths.output, 'assets'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+    new Dotenv(),
+  ].concat(htmlPluginEntries),
+  target: 'web',
+};
