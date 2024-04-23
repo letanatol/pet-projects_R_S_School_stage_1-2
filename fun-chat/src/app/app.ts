c643ac87c9d0cc3729bc7faa16be176c11b800e7
letanatol <letanatol@gmail.com>
2024-04-23 22:03:07 +0300

    fix: delete addEventListener for getState in console

--- fun-chat/src/app/app.ts
+++ fun-chat/src/app/app.ts
@@ -1,15 +1,15 @@
 import { MainContainer } from '@components/MainContainer/MainContainer';
 import { Modal } from '@components/modalCloseServer/modalCloseServer';
-import { state } from '@helpers/State/State';
+// import { state } from '@helpers/State/State';
 
 export class App {
   constructor() {
     const { body } = document;
     const mainContainer = new MainContainer();
     const modalContainer = new Modal();
-    body.addEventListener('click', () => {
-      state.getState();
-    });
+    // body.addEventListener('click', () => {
+    //   state.getState();
+    // });
 
     body.append(mainContainer.init(), modalContainer.init());
   }
